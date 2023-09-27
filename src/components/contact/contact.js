60332cf578ab7aa616d4a0ebbbec8f8b0dfadd78
marcosfacchetti <marcos-facchetti@hotmail.com>
2023-09-27 16:38:17 -0300

    w

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -29,8 +29,8 @@ const Contact = () => {
             <h1 className='contactTitulo'>Contact Me</h1>
             <span className='contactDesc'>Please fill out the form below to discuss any work opportunities.</span>
             <form className='contactForm' ref={form} onSubmit={sendEmail} >
-                <input className='name' type='text' placeholder='Your Name' name="your_name"/>
-                <input className='email' type='email' placeholder='Your Email' name="your_email"/>
+                <input className='name' type='text' placeholder='Your Name' name="from_name"/>
+                <input className='email' type='email' placeholder='Your Email' name="from_email"/>
                 <textarea className='msj' name='message' rows="5" placeholder='Your Message'/>
                 <button className='submitBtn' type='submit' value="Send">Submit</button>
             </form>
