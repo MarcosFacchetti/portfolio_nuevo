75bf6a956a08a4347c3075418eabbc3ef56c8370
marcosfacchetti <marcos-facchetti@hotmail.com>
2023-09-28 04:43:37 -0300

    boton cv

--- src/components/inicio/inicio.js
+++ src/components/inicio/inicio.js
@@ -1,20 +1,22 @@
-import React from 'react'
-import "./inicio.css"
-import bg from "./bg_1.png"
-import { Link } from "react-scroll"
-import hire from "./hire.png"
+import React from 'react';
+import "./inicio.css";
+import bg from "./bg_1.png";
+import cv from "./cv.pdf";
 
 
 const Inicio = () => {
+
+  const abrirCV = () => {
+    window.open(cv, '_blank');
+  };
+
   return (
     <section id='inicio'>
       <div className='introContent'>
         <span className='hello'>Hello,</span>
         <span className='introText'>I'm <span className='introName'>Marcos</span> <br/> Fullstack developer </span>
         <p className='introPara'>Skilled web designer whit experience in creating<br/>visually appealing and user friendly websites.</p>
-        <Link><button className='btn'onClick={() => {
-          document.getElementById("contact").scrollIntoView({behavior: "smooth"});
-        }}><img src={hire} alt='maleta' className='btnImg'/>Hire me</button></Link>
+        <button className='btn' onClick={abrirCV}>View CV</button>
       </div>
       <img src={bg} alt='Profile' className='bg' />
     </section>
